79d0fd6a13a01eff9f425272036bf36bb59cc7f7
opilane <opilane@HP-CZC6297B92>
2020-09-10 09:18:22 +0300

    SliderStepper

---
+++ mobil_shevchenko/mobil_shevchenko/SliderSepper.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobil_shevchenko
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SliderSepper : ContentPage
+    {
+        public SliderSepper()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Editor1.FontSize = Slider1.Value;
+        }
+
+        private void Slider1_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor1.FontSize = e.NewValue;
+                Stepper1.Value = e.NewValue;
+            }
+
+        }
+
+        private void Stepper1_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+                Editor1.FontSize = e.NewValue;
+            Slider1.Value = e.NewValue;
+        }
+    }
+}
