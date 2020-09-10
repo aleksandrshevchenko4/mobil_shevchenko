0e5d692777830cd4d9f5ea63292107574c6e9f53
opilane <opilane@HP-CZC6297B92>
2020-09-10 10:36:42 +0300

    editor

--- mobil_shevchenko/mobil_shevchenko/SliderSepper.xaml.cs
+++ mobil_shevchenko/mobil_shevchenko/SliderSepper.xaml.cs
@@ -21,6 +21,8 @@ namespace mobil_shevchenko
             base.OnAppearing();
 
             Editor1.FontSize = Slider1.Value;
+            Editor2.FontSize = Slider2.Value;
+            Editor3.FontSize = Slider3.Value;
         }
 
         private void Slider1_ValueChanged(object sender, ValueChangedEventArgs e)
@@ -36,8 +38,46 @@ namespace mobil_shevchenko
         private void Stepper1_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             if (e.NewValue > 0)
+            {
                 Editor1.FontSize = e.NewValue;
-            Slider1.Value = e.NewValue;
+                Slider1.Value = e.NewValue;
+            }
+        }
+
+        private void Slider2_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor2.FontSize = e.NewValue;
+                Stepper2.Value = e.NewValue;
+            }
+        }
+
+        private void Stepper2_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor2.FontSize = e.NewValue;
+                Slider2.Value = e.NewValue;
+            }
+        }
+
+        private void Slider3_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor3.FontSize = e.NewValue;
+                Stepper3.Value = e.NewValue;
+            }
+        }
+
+        private void Stepper3_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor3.FontSize = e.NewValue;
+                Slider3.Value = e.NewValue;
+            }
         }
     }
 }
